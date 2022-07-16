fe58c40cb384ab5a8fcd45c4238e22f96f140df9
lucassrondon <lucas.rondon@hotmail.com>
2022-07-16 17:45:52 -0300

    mudancas nos arquivos

--- sistema-biblioteca/admin/index.php
+++ sistema-biblioteca/admin/index.php
@@ -3,7 +3,7 @@
 <?php include '../includes/functions.inc.php'; ?> <!-- PHP FUNCTIONS -->
 <?php include '../includes/check_if_admin.inc.php'; ?> <!-- CHECKS IF WHO IS ACCESSING THIS FILE IS admin-->
 <?php include '../includes/logout.inc.php'; ?> <!-- LOGOUT SCRIPT -->
-<?php include '../includes/adminindex.inc.php'; ?>
+<?php include '../includes/adminindex.inc.php'; ?> <!-- THIS PAGE'S SCRIPT -->
 
 <!DOCTYPE html>
 <html lang="pt-BR">
@@ -59,20 +59,37 @@
             </div>
 
             <?php
+                #THE NUMBER OF ITEM ROWS WILL BE THE AMOUNT OF ITEMS->
+                #TO BE SHOWN PER PAGE DIVIDED BY 3, BECAUSE EACH ROW->
+                #WILL HAVE 3 ITEMS
                 $number_of_book_rows = $items_per_page / 3;
 
                 echo "<books>";
+
                     for($row = 1; $row <= $number_of_book_rows; $row++){
+
                         echo "<linha>";
+
                             for($book = 1; $book <= 3; $book++){
+                                #IN EACH ITERATION, A DIFFERENT BOOK INFO IS->
+                                #->GOTTEN FROM THE VARIABLE RESULT, WHICH HOLDS->
+                                #->THE INFO OF THE BOOKS THAT WERE REQUESTED
                                 $sql_row = mysqli_fetch_array($result);
+
                                 if ($sql_row){
                                     echo "<book>";
                                         if($sql_row['imagemtipo']){
-                                            echo "<img src='../imagens/" . $sql_row['id'] . '.' . $sql_row['imagemtipo'] . "'" . ">";
+                                            #IF THE imagemtipo FIELD IS NOT EMPTY, IT MEANS->
+                                            #->THAT THERE IS AN IMAGE FOR THE BOOK, AND THE->
+                                            #->IMAGE'S NAME IS THE BOOK ID PLUS THE IMAGE TYPE
+                                            echo "<img src='../imagens/" . $sql_row['id'] . 
+                                                    '.' . $sql_row['imagemtipo'] . "'" . ">";
                                         }
                                         else{
-                                            echo "<img src='../imagens/book.jpg'>";
+                                            #IF THERE IS NOTHING IN THE FIELD imagemtipo->
+                                            #IT MEANS THAT THE BOOK HAS NO IMAGE, SO A ->
+                                            #DEFAULT IMAGE IS SHOWN
+                                            echo "<img src='../imagens/book.jpg'>"; 
                                         }
                                         echo "<book-description>";
 
@@ -89,15 +106,19 @@
                                     echo "</book>";
                                 }
                             }
+
                         echo "</linha>";
+
                     }
+
                 echo "</books>";
 
             ?>
 
             <?php
                 for($page=1; $page <= $number_of_pages; $page++){
-                    echo "<a href=" . $_SERVER['PHP_SELF'].'?page=' . $page . ">" . " " . $page . "</a>";
+                    echo "<a href=" . $_SERVER['PHP_SELF'].'?page=' . 
+                            $page . ">" . " " . $page . "</a>";
                 }
             ?>
         </section>

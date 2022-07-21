fa42d457c9dcb05b3607751e3910c2cfa6417253
Karoline Terra <89556554+terrakaroline@users.noreply.github.com>
2022-07-21 11:23:10 -0300

    estilo e estrutura parcial da página de livro

--- sistema-biblioteca/admin/livro.php
+++ sistema-biblioteca/admin/livro.php
@@ -7,31 +7,69 @@
 
 <!DOCTYPE html>
 <html lang="pt-BR">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Livro</title>
-</head>
-<body>
-    <?php
-        if (isset($img_path)){echo "<img src='$img_path' width='120px' height='170px'>";}
-        if (isset($book_title)){echo "<p>Titulo: $book_title</p>";}
-        if (isset($book_author)){echo "<p>Autor: $book_author</p>";}
-        if (isset($book_pages)){echo "<p>Paginas: $book_pages</p>";}
-        if (isset($book_genre)){echo "<p>Categoria: $book_genre</p>";}
-        if (isset($book_copies)){echo "<p>Exemplares: $book_copies</p>";}
-        if (isset($book_borrowed)){echo "<p>Emprestados: $book_borrowed</p>";}
-        if (isset($book_description)){echo "<p>Descricao: $book_description</p>";}
-    ?>
-    <p>Retirado por: XXXXXXX Em:XXXXX Data devolucao: XXXXX Atrasado:Sim/Nao</p>
-    
-    <form action="<?php echo $_SERVER['PHP_SELF'].'?livro=' . $book_id;?>" method="post">
-        <input type="email" name="student_email" id="">
-        <input type="submit" value="Enviar">
-    </form>
-    <div><?php if(isset($lending_error)){echo $lending_error;}?></div>
-    <?php print_r($_POST); ?>
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Livro</title>
+        <link rel="stylesheet" href="../css/livro.css">
+    </head>
+    <body>
+        <section class="space"></section>
+        <section class="menu">
+            <menu-user-image>
+                <img src="imagens/user.png">
+            </menu-user-image>
+            <menu-user-name>
+                Nome do usuário
+                <menu-user-acess>
+                    Administrador <br>
+                    <?php
+                        
+                    ?>
+                <menu-user-acess>
+            </menu-user-name>
+            
+            <menu-item>
+                <a href="<?php echo $_SERVER['PHP_SELF'].'?logout=1';?>">Encerrar Sessao</a>
+            </menu-item>
+            <menu-item>
+                <a href="index.php">Início</a>
+            </menu-item>
+            <menu-item>
+                <a href="./cadastrar.php">Cadastrar Aluno</a>
+            </menu-item>
+            <menu-item>
+                <a href="./cadastroLivros.php">Cadastrar Livro</a>
+            </menu-item>
+            <menu-item>
+                Item X
+            </menu-item>
+            <menu-item>
+                Item X
+            </menu-item>
+        </section>
+        <div class="infolivro">
+            <?php
+                if (isset($book_title)){echo "<h1 class='titulo'>$book_title</h1>";}
+                if (isset($img_path)){echo "<div class='ladinho'><img src='$img_path' width='240px' height='340px'>";}
+                if (isset($book_description)){echo "<div class='informacoes_livro'> <p class='conteudo'><span class='bold'>Descricao: </span>$book_description</p>";}
+                if (isset($book_author)){echo "<p class='conteudo'><span class='bold'>Autor:</span> $book_author</p>";}
+                if (isset($book_pages)){echo "<p class='conteudo'><span class='bold'>Paginas:</span> $book_pages</p>";}
+                if (isset($book_genre)){echo "<p class='conteudo'><span class='bold'>Categoria:</span> $book_genre</p>";}
+                if (isset($book_copies)){echo "<p class='conteudo'><span class='bold'>Exemplares:</span> $book_copies</p>";}
+                if (isset($book_borrowed)){echo "<p class='conteudo'><span class='bold'>Emprestados:</span> $book_borrowed</p></div></div>";}
+            ?>
+            <p class='conteudovar'><span class='bold'>Retirado por:</span> XXXXXXX <span class='bold'>Em:</span>XXXXX <span class='bold'>Data devolucao:</span> XXXXX <span class='bold'>Atrasado:</span> Sim/Nao</p>
 
-</body>
+            <form action="<?php echo $_SERVER['PHP_SELF'].'?livro=' . $book_id;?>" method="post">
+            
+            <input type="email" name="student_email" id="">
+            <input type="submit" value="Enviar">
+            </form>
+            <div><?php if(isset($lending_error)){echo $lending_error;}?></div>
+            <?php print_r($_POST); ?>
+        </div>
+
+    </body>
 </html>
